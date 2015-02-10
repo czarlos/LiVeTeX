00c072aab263c71aff85331a8375fcb1b817ee7a
czarlos <cer26@duke.edu>
2015-02-09 20:51:58 -0500

    initial version of livetex

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var qs = require('querystring');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res) {	
+	res.render('index', {title: 'Express'});	
+});
+
+router.post('/', function(req, res) {
+	var l = req.param('latex');
+	console.log(l);
+	if (l!== undefined) {
+		myFile = fs.createWriteStream('public/myOutput.pdf');
+		var buffer = require('latex')(l).on('error', function(err) {
+			console.log("oops");
+			return;
+		});
+		buffer.pipe(myFile);
+		buffer.on('end', function() {
+			myFile.end();
+			res.json({completed: true});
+		});
+	}
+	else {
+		res.status(400).json({msg: 'you are fool'});	
+	}	
+});
+
+module.exports = router;
